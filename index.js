71f312ceb0dafe765b9c5c1f90a96989443e8866
alexgerasimov58rus <vintegewhitecool@yandex.ru>
2021-05-25 15:19:27 +0300

    Исправил замечания по первому ДЗ

--- index.js
+++ index.js
@@ -3,31 +3,39 @@ const colors = require("colors/safe");
 
 main();
 
-function main() {
-    let range = [];
-    if (checkInputArgumentsAndInitRange(range)){
-        let countSimpleNumbers = 0;
-        for(let i = range[0]; i <= range[1] && countSimpleNumbers < 3; i++){
-            if( isPrime(i)){
-                switch (countSimpleNumbers % 3) {
-                    case 0: console.log(colors.green(i)); break;
-                    case 1: console.log(colors.yellow(i)); break;
-                    case 2: console.log(colors.red(i)); break;
-                }
-                countSimpleNumbers++;
-            }
-        }
-
-        if( countSimpleNumbers === 0){
-            console.log(colors.red("В заданном диапазоне нет простых чисел"));
-        }
+function checkInputArguments(argv) {
+    if( argv.length !== 4 ){
+        console.error("Вы не передали диапазон при вызове скрипта");
+        return false;
     }
+
+    const a = Number.parseInt(argv[2]);
+    const b = Number.parseInt(argv[3]);
+
+    if( isNaN(a) || isNaN(b) || a < 0 || b < 0){
+        console.error("Вы передали некорректные параметры при вызове скрипта. Скрипт принимет на вход 2 положительных числа");
+        return false;
+    }
+
+    return true;
+}
+
+function getRange(argv) {
+    const range = [];
+
+    const a = Number.parseInt(argv[2]);
+    const b = Number.parseInt(argv[3]);
+
+    range[0] = Math.min(a, b);
+    range[1] = Math.max(a, b);
+
+    return range;
 }
 
 function isPrime(v) {
     if(v < 2) return false;
 
-    let n = Math.floor(Math.sqrt(v + 0.5));
+    const n = Math.floor(Math.sqrt(v + 0.5));
     for (let i = 2; i <= n; i++) {
         if( v % i === 0) {
             return false;
@@ -38,20 +46,32 @@ function isPrime(v) {
     return true;
 }
 
-function checkInputArgumentsAndInitRange(range) {
-    if( process.argv.length !== 4 ){
-        console.error("Вы не передали диапазон при вызове скрипта");
-        return false;
-    }
-    let a = Number.parseInt(process.argv[2]);
-    let b = Number.parseInt(process.argv[3]);
+function main() {
+    if (!checkInputArguments(process.argv)) return;
+    const [rangeStart, rangeEnd] = getRange(process.argv);
 
-    if( isNaN(a) || isNaN(b) || a < 0 || b < 0){
-        console.error("Вы передали некорректные параметры при вызове скрипта. Скрипт принимет на вход 2 положительных числа");
-        return false;
+    let countSimpleNumbers = 0;
+    for(let i = rangeStart; i <= rangeEnd; i++){
+        if( isPrime(i)){
+            switch (countSimpleNumbers % 3) {
+                case 0: console.log(colors.green(i)); break;
+                case 1: console.log(colors.yellow(i)); break;
+                case 2: console.log(colors.red(i)); break;
+            }
+            countSimpleNumbers++;
+        }
+
+        if (countSimpleNumbers === 3) {
+            break;
+        }
     }
 
-    range[0] = Math.min(a, b);
-    range[1] = Math.max(a, b);
-    return true;
+    if( countSimpleNumbers === 0){
+        console.log(colors.red("В заданном диапазоне нет простых чисел"));
+    }
 }
+
+
+
+
+

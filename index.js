de3e5cd6356645fce5f53ab7c623f39077531ee9
alexgerasimov58rus <vintegewhitecool@yandex.ru>
2021-06-01 18:53:59 +0300

    Сделал через с привлечением событий. Через одно место, как мне кажется. Просто притянул за уши, лишь бы были

--- index.js
+++ index.js
@@ -8,6 +8,10 @@
 // Record 4
 
 /* Задание 2 */
+const events = require('events');
+
+const source = new events.EventEmitter();
+
 function parseDate(strDate) {
     const arr = strDate.split('-');
     if( arr.length !== 4) return null;
@@ -29,23 +33,60 @@ function Timer(id, strDate){
     let date = parseDate(strDate);
     if( date === null) return;
 
-    let seconds = Math.round((date.getTime() - Date.now()) / 1000);
+    let mSeconds = Math.round((date.getTime() - Date.now())/1000) * 1000;
 
-    return function foo() {
-        seconds--;
-        if( seconds > 0){
-            console.log("Timer " + id + ": " + seconds);
-            setTimeout(foo, 1000);
+    return function foo(deltaMSeconds) {
+        mSeconds -= deltaMSeconds;
+
+        if( mSeconds > 0){
+            console.log("Timer " + id + ": " + Math.round(mSeconds / 1000));
         } else{
             console.log("Timer " + id + ": завершил работу")
+            return true;
         }
+
+        return false;
     }
 }
+
+const timers = [];
+let idTimer = 0;
+let timePrev;
+
+main();
 
 function main() {
     for(let i = 2; i < process.argv.length; i++){
-        setTimeout(Timer(i-1, process.argv[i]));
+        const timer = Timer(i-1, process.argv[i]);
+        if (timer !== null){
+            timers.push(timer);
+        }
     }
+
+    source.on('update', (deltaMSeconds)=>{
+        for (let i=0; i < timers.length; i++){
+            if( timers[i](deltaMSeconds)){
+                timers.splice(i, 1);
+                i--;
+            }
+        }
+    });
+
+    timePrev = Date.now();
+    idTimer = setInterval(updateInterval, 1000);
 }
 
-main();
+function updateInterval() {
+    if( timers.length === 0){
+        clearInterval(idTimer);
+        return;
+    }
+
+    const deltaMSeconds = Date.now() - timePrev;
+    timePrev = Date.now();
+
+    source.emit('update', deltaMSeconds);
+}
+
+
+
